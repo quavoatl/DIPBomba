149e23a63cbcce3f19c5aaf54fc30b61b1284dc1
Sergiu Alexandru Antal <sergiu.antal@endava.com>
2020-04-27 23:56:12 +0300

    Proxy pattern

---
+++ ProxyPattern/IDatabaseManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyPattern
+{
+    public interface IDatabaseManager
+    {
+        public User CurrentUser { get; set; }
+        void ExecuteSomeQuery(string query);
+    }
+}

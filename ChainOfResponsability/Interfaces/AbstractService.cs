48c70ee4b0b93809229c1833ef11555b0e08dfeb
Sergiu Alexandru Antal <sergiu.antal@endava.com>
2020-07-10 22:00:11 +0300

    Added Chain Of Responsability Pattern

---
+++ ChainOfResponsability/Interfaces/AbstractService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsability.Interfaces
+{
+    public abstract class AbstractService
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public AbstractService(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ ChainOfResponsability/Program.cs
@@ -0,0 +1,30 @@
+﻿using ChainOfResponsability.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ChainOfResponsability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractService carWashService = new CarWashService("Washing your car", 15.5);
+            AbstractService carTyresService = new CarTyreService("Fixing your tyres", 20.75);
+            AbstractService carEngineTuneService = new CarEngineService("Tuning your engine", 50.25);
+
+            AbstractServiceHandler carTyresHandler = new CarTyresServiceHandler(carTyresService);
+            AbstractServiceHandler carEngineTuneHandler = new CarEngineTuneHandler(carEngineTuneService);
+            AbstractServiceHandler carWasherHandler = new CarWasherHandler(carWashService);
+
+            AbstractCar toyota = new Toyota("Toyota Avensis 2.2D", new List<AbstractService>(new AbstractService[] { carWashService,carTyresService}));
+
+            List<AbstractServiceHandler> listOfServiceHandlers =
+                new List<AbstractServiceHandler>(new AbstractServiceHandler[] { carTyresHandler, carEngineTuneHandler, carWasherHandler });
+
+            ServiceCenter serviceCenter = new ServiceCenter(listOfServiceHandlers);
+            serviceCenter.StartServiceProcess(toyota);
+
+
+        }
+    }
+}

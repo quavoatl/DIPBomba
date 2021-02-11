73550ce7696cfb2ca57f4911de630006cdd6de1d
Sergiu Alexandru Antal <sergiu.antal@endava.com>
2021-02-11 23:29:51 +0200

    added new chain of responsability pattern

---
+++ EmailServiceClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UniversityEmailProcessor;
+
+namespace EmailServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmailService emailService = new UniversityEmailService();
+            emailService.SendRequest("cacat");
+
+        }
+    }
+}
---
+++ UniversityEmailProcessor/UniversityEmailService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityEmailComponent;
+
+namespace UniversityEmailProcessor
+{
+    public class UniversityEmailService : IEmailService
+    {
+        private IUniversityEmailManager _firstHandler;
+
+        private List<IUniversityEmailManager> _handlers = new List<IUniversityEmailManager>(new IUniversityEmailManager[]
+        {
+            new HREmailManager(),
+            new FinanceEmailManager(),
+            new AdminEmailManager()
+        });
+       
+        public UniversityEmailService()
+        {
+            //_firstHandler = new HREmailManager();
+            //var _secondHandler = new FinanceEmailManager();
+            //var _thirdHandler = new AdminEmailManager();
+            //_firstHandler.SetNextHandler(_secondHandler);
+            //_secondHandler.SetNextHandler(_thirdHandler);
+            SetChain();
+        }
+
+        private void SetChain()
+        {
+            for (int i = 0; i < _handlers.Count-1; i++)
+            {
+                try
+                {
+                    _handlers[i].SetNextHandler(_handlers[i + 1]);
+                }
+                catch(Exception ex) { }
+            }
+        }
+
+        public void SendRequest(string request)
+        {
+            foreach (var handler in _handlers)
+            {
+                handler.ProcessRequest(request);
+
+                if (handler.RequestProcessed) break;
+            }
+        }
+    }
+}
